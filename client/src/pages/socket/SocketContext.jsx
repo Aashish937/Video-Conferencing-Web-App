6e4b2cde16b8bcdc5b6b18deef2ed037daf32675
Aashish937 <2k22.csai.32851@gmail.com>
2025-07-22 13:51:23 +0530

    final

--- client/src/pages/socket/SocketContext.jsx
+++ client/src/pages/socket/SocketContext.jsx
@@ -4,7 +4,7 @@ let socket;
 
 const getSocket = () => {
     if (!socket) {
-        socket = io('https://video-conferencing-web-app-server.onrender.com');
+        socket = io(import.meta.env.VITE_API_SOCKET_URL);
     }
     return socket;
 }
