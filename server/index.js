042c27372052856b51eab603156c9ad0285db24b
Aashish937 <2k22.csai.32851@gmail.com>
2025-07-24 07:21:13 +0530

    a

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 3000;
 const server = createServer(app);
 
 // 🌍 Allowed frontend origins for CORS (Cross-Origin Resource Sharing)
-const allowedOrigins = [process.env.CLIENT_URL];
+const allowedOrigins = [process.env.FRONTEND_URL];
 console.log(allowedOrigins); // Debugging: Check if the frontend URL is loaded properly
 
 // 🔧 Middleware to handle CORS
