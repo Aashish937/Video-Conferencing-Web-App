5b4eae2c2744e41ef0859ea11894915e3283002b
Aashish Kumar Singh <145109905+Aashish937@users.noreply.github.com>
2025-10-14 10:54:57 +0530

    Update jwtToken.js

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -8,7 +8,7 @@ const jwtToken =  (userId , res)=>{
         maxAge: 30 *24 *60 *60 *1000,
         httpOnly:true,
         sameSite:"None",
-        secure:true,
+        secure:process.env.NODE_ENV === 'production',
         path: '/' 
     })
     return token;
