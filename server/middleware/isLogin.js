e71c79cd36de2765ff120e237a254b1cd01e2d51
Aashish937 <2k22.csai.32851@gmail.com>
2025-10-14 11:16:46 +0530

    error

--- server/middleware/isLogin.js
+++ server/middleware/isLogin.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken'
 import User from '../schema/userSchema.js'
 
-const isLogin = (req, res, next) => {
+const isLogin = async(req, res, next) => {
     try {
-        const token = req.cookies.jwt || req.headers.cookie.split("; ").find((cookie) => cookie.startsWith("jwt="))?.split("=")[1];
+        const token = req.cookies?.jwt || req.headers.cookie?.split("; ").find((cookie) => cookie.startsWith("jwt="))?.split("=")[1];
         //console.log(token);
         if (!token) return res.status(500).send({ success: false, message: "User Unauthorize" });
         const decode = jwt.verify(token,process.env.JWT_SECRET);
