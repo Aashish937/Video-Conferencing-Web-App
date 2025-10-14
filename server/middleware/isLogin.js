82b52cf154b081ebd730b7076a00ac6d31d20615
Aashish937 <2k22.csai.32851@gmail.com>
2025-10-14 17:52:58 +0530

    server

--- server/middleware/isLogin.js
+++ server/middleware/isLogin.js
@@ -3,15 +3,14 @@ import User from '../schema/userSchema.js'
 
 const isLogin = async(req, res, next) => {
     try {
-        const token = req.cookies?.jwt || req.headers.cookie?.split("; ").find((cookie) => cookie.startsWith("jwt="))?.split("=")[1];
-        console.log("token : ",token);
+        const token = req.cookies.jwt || req.headers.cookie.split("; ").find((cookie) => cookie.startsWith("jwt="))?.split("=")[1];
         //console.log(token);
         if (!token) return res.status(500).send({ success: false, message: "User Unauthorize" });
         const decode = jwt.verify(token,process.env.JWT_SECRET);
-        if(!decode)  return res.status(500).send({success:false, message:"User Unauthorize -Invalid Token"})
+        if(!decode)  return res.status(500).send({success:false, message:"User Unauthorize -Invalid Token"});
         const user = User.findById(decode.userId).select("-password");
         if(!user) return res.status(500).send({success:false, message:"User not found"})
-        req.user = user,
+        req.user = user;
         next()
     } catch (error) {
         console.log(`error in isLogin middleware ${error.message}`);
